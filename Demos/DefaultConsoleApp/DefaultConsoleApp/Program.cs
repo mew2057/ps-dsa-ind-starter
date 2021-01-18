a6744b3ce26b9bae99a18f1fe69ff45835c83335
emesh <erika.s.mesh@gmail.com>
2021-01-18 14:41:53 -0500

    Setup for Spring 2021

---
+++ Demos/DefaultConsoleApp/DefaultConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GDAPS2!");
+        }
+    }
+}
---
+++ Demos/README_Demos.md
@@ -0,0 +1,2 @@
+# Demo Projects
+Put demo projects provided to you or that you create for yourself here.

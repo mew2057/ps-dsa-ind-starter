1f2c57d495c54200abc7b886981435787ed99350
Erika Mesh <esmvcs@rit.edu>
2023-01-14 09:53:54 -0500

    Added the console compare demo

---
+++ Demos/ConsoleCompare_Demo/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ConsoleCompare_Demo
+{
+    /// <summary>
+    /// This project demos the use of Prof. Chris Cascioli's 
+    /// new ConsoleCompare extension for VS 2022: https://github.com/vixorien/ConsoleCompare
+    /// 
+    /// To use it:
+    ///  1. Download the vsix file from latest release: https://github.com/vixorien/ConsoleCompare/releases
+    ///  2. Install the vsix file
+    ///  3. In Visual Studio, go to Extensions -> Console Compare -> Open Console Compare Window
+    ///  4. Load the .simile file in the Console Compare window
+    ///  5. Click Run in the console compare window (the |> icon)
+    /// 
+    /// When possible, I'll give you .simile files to go with PEs and Homeworks so you can test your
+    /// output against what we expect. These in NO WAY should replace you manually testing your
+    /// work! The tests we give you will NOT cover every possible situation!
+    /// 
+    /// ConsoleCompare also checks how many classes and methods have XML headers like this
+    /// (Create them by typing 3 slashes "///" on the line above a class or method declaration.)
+    /// We WILL be using this when grading homeworks and practicals as a check for how many
+    /// comment headers you have!
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main has some hard-coded output + output dependent on an input string.
+        /// 
+        /// It also prints a random number that might be outside the range the .simile
+        /// file expects so that you can see what a failed test looks like.
+        /// 
+        /// Finally, it reads and lists the contents of a file (mostly to make sure
+        /// file io works with the extension).
+        /// 
+        /// NOTE that ConsoleCompare is VERY picky about output matching exactly.
+        /// Extra whitespace of any kind (new line, tabs vs spaces, etc.) will make
+        /// the tests fail. And if the whitespace means input is requested on a line
+        /// that doesn't have a {{ }} input marker in the .simile file, ConsoleCompare
+        /// will hang.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Variables we'll need
+            string name;
+            StreamReader input = null;
+            Random rng = new Random();
+
+            // Greeting
+            Console.WriteLine("Hello GDAPS!\n");
+
+            // Get and use a name
+            name = SmartConsole.GetPromptedInput("Who are you?");
+            Console.WriteLine($"\nHello {name.ToUpper()}!\n" +
+                "This is pretty cool!\n");
+
+            // Should print out a random # from 7 to 42
+            // Intentionally going WAY out of range so sometimes this
+            // line fails the compare test
+            Console.WriteLine("Your random ID is " + rng.Next(7, 100)+"\n");
+
+            // Test file IO
+            Console.WriteLine("InputFile contents:");
+            try
+            {
+                input = new StreamReader("../../../InputFile.txt");
+                string line = null;
+                while((line = input.ReadLine()) != null)
+                {
+                    Console.WriteLine(" - " + line);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Uhoh: " + e.Message);
+            }
+            
+        }
+    }
+}
---
+++ Demos/ConsoleCompare_Demo/SmartConsole.cs
@@ -0,0 +1,146 @@
+﻿// CHANGE the namespace to match the one in your project!
+namespace ConsoleCompare_Demo
+{
+    /// <summary>
+    /// Author: Erika S. Mesh. Demo/helper code written for RIT GDAPS 1 and 2.
+    /// 
+    /// The SmartConsole class provides "helper" methods to perform common
+    /// behaviors for us. This keeps code elsewhere in the project simpler.
+    /// 
+    /// All of the methods in this class are defined as "static". This isn't 
+    /// a keyword we discussed in GDAPS1 and it won't be on the exam, but 
+    /// it's worth being aware of.
+    /// 
+    /// Static methods are defined in a class, but don't require a specific 
+    /// object (instance of the class) in order to execute them. Instead,
+    /// we call them using the class type. I.e. SmartConsole.<method name>.
+    /// 
+    /// This is how the Math and Console methods you are used to using work.
+    /// </summary>
+    class SmartConsole
+    {
+        private static ConsoleColor DefaultColor = ConsoleColor.White;
+        private static ConsoleColor InputColor = ConsoleColor.Cyan;
+
+
+        /// <summary>
+        /// This enum is defined *inside* the class as private
+        /// because I don't intend for any other class to use it.
+        /// It's a "helper" for the GetYesNoInput.
+        /// </summary>
+        private enum YesNoParse
+        {
+            Yes,
+            No,
+            Invalid
+        }
+
+        /// <summary>
+        /// Displays the given prompt, waits for input, and
+        /// returns the trimmed input.
+        /// </summary>
+        /// <param name="prompt">A string prompt to display to the user.</param>
+        /// <returns>Trimmed string</returns>
+        public static string GetPromptedInput(string prompt)
+        {
+            Console.ForegroundColor = DefaultColor;
+            Console.Write(prompt + " ");
+            Console.ForegroundColor = InputColor;
+            string input = Console.ReadLine().Trim();
+            Console.ForegroundColor = DefaultColor;
+            return input;
+        }
+
+        /// <summary>
+        /// Displays the given prompts, waits for input, and
+        /// tries to parse the input as an int. Loops until the
+        /// provided input is a valid int within the range given (inclusive)
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int GetIntRangeInput(string prompt, int min, int max)
+        {
+            int result = int.MinValue;
+            bool success = int.TryParse(GetPromptedInput(prompt), out result);
+            while (!success || result < min || result > max)
+            {
+                success = int.TryParse(
+                    GetPromptedInput(String.Format("Please enter a whole number in the range {0}-{1}: ", min, max)), 
+                    out result);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Displays the given prompts, waits for input, and
+        /// tries to parse the input as a double. Loops until the
+        /// provided input is a valid double within the range given (inclusive)
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static double GetDoubleRangeInput(string prompt, double min, double max)
+        {
+            double result = double.MinValue;
+            bool success = double.TryParse(GetPromptedInput(prompt), out result);
+            while (!success || result < min || result > max)
+            {
+                success = double.TryParse(
+                    GetPromptedInput(String.Format("Please enter a whole number in the range {0}-{1}: ", min, max)),
+                    out result);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Internal helper method to convert a string input to
+        /// our enum value. Not really neccessary (a compound if with strings
+        /// would be fine), but I wanted to now this switch statement.
+        /// </summary>
+        /// <param name="choice"></param>
+        /// <returns></returns>
+        private static YesNoParse ParseYesNoChoice(string choice)
+        {
+            choice = choice.ToLower();
+            YesNoParse result = YesNoParse.Invalid;
+            switch (choice)
+            {
+                case "yes":
+                case "y":
+                    result = YesNoParse.Yes;
+                    break;
+
+                case "no":
+                case "n":
+                    result = YesNoParse.No;
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter Yes, No, Y or N.");
+                    break;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Display the given prompt and return a bool based on a
+        /// yes/y/no/n input (and doesn't take anything else)
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <returns>True if yes</returns>
+        public static bool GetYesNoInput(string prompt)
+        {
+            string choice = GetPromptedInput(prompt);
+            YesNoParse result = ParseYesNoChoice(choice);
+            while (result == YesNoParse.Invalid)
+            {
+                choice = GetPromptedInput(prompt);
+                result = ParseYesNoChoice(choice);
+            }
+            return result == YesNoParse.Yes;
+        }
+    }
+}
